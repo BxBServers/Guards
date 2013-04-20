41eb481408543dbbdf83df18e730d28e50a47c44
joncpaske <joncpaske@129.11.199.80>
2013-04-20 16:08:03 +0100

    start editing DeathListener

--- src/com/bxbservers/Guards/listeners/DeathListener.java
+++ src/com/bxbservers/Guards/listeners/DeathListener.java
@@ -17,70 +17,106 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 import com.bxbservers.Guards.Guards;
 
-public class DeathListener implements Listener {
-
+public class DeathListener implements Listener
+{
 	private Guards plugin;
 	Random generator = new Random();
 	
-	public DeathListener(Guards instance){
+	
+	public DeathListener(Guards instance)
+	{
 		this.plugin = instance;
 	}
 	
-	//Remove Death Messages
+	
+	//Clearing Death Messages
 	@EventHandler
-	public void death(PlayerDeathEvent e){
-			e.setDeathMessage("");
+	public void death(PlayerDeathEvent e)
+	{
+		e.setDeathMessage("");
 	}
+	
 	
 	@SuppressWarnings("incomplete-switch")
 	@EventHandler
-	public void onDeath(EntityDeathEvent e) {		
+	public void onDeath(EntityDeathEvent e)
+	{		
+		//gets player who killed
 		Player killer = e.getEntity().getKiller();
-		if (killer == null) {
-			switch (e.getEntityType()) {
+		
+		//if not killed by another player
+		if (killer == null) 
+		{
+			switch (e.getEntityType())
+			{
+			//Guard drop items are cleared
 			case PLAYER:
 				Player victim = (Player)e.getEntity();
-				if (plugin.onDuty.contains(victim.getName())) {
+				if (plugin.onDuty.contains(victim.getName()))
+				{
 					e.getDrops().clear();
 				}
 			}
 			return;
 		}
-		switch (e.getEntityType()) {
-		case PLAYER:
-			Player victim = (Player)e.getEntity();
-			if (plugin.onDuty.contains(victim.getName())) {
-				e.getDrops().clear();
-				if (plugin.getConfig().getBoolean("dropGuardHeads")) {
-					plugin.logger.info("Head Dropping Enabled");
-					ItemStack Skull = new ItemStack(Material.SKULL_ITEM, 1, (short)SkullType.PLAYER.ordinal());
-					SkullMeta skullMeta = (SkullMeta)Skull.getItemMeta();
-					skullMeta.setOwner(victim.getName());
-					Skull.setItemMeta(skullMeta);
-					e.getDrops().add(Skull);
+				
+		
+		switch (e.getEntityType()) 
+		{
+			case PLAYER:
+				
+				Player victim = (Player)e.getEntity();
+				
+				if (plugin.onDuty.contains(victim.getName()))
+				{
+					//clears dropped
+					e.getDrops().clear();
 					
-		            EconomyResponse r = Guards.econ.depositPlayer(killer.getName(), plugin.getConfig().getInt("moneyOnGuardKill"));
-		            if(r.transactionSuccess()) {
-		               killer.sendMessage(String.format("You were rewarded %s for killing a guard", Guards.econ.format(r.amount)));
-		            } else {
-		                killer.sendMessage(String.format("An error occured: %s", r.errorMessage));
-		            }
-					
-					for(Player user: plugin.getServer().getOnlinePlayers()) {
-						if (!(plugin.onDuty.contains(user.getName()))) {
-							user.sendMessage(plugin.prefix +killer.getName() + " has just killed guard " + victim.getName());
-						}
+					//if guard head drop is set to true
+					if (plugin.getConfig().getBoolean("dropGuardHeads"))
+					{
+						//creates single human skull
+						ItemStack Skull = new ItemStack(Material.SKULL_ITEM, 1, (short)SkullType.PLAYER.ordinal());
 						
-					}
-					return;
-				} else {
-					plugin.logger.info("Head Dropping Disabled");
-				}		
+						//gets stacks information
+						SkullMeta skullMeta = (SkullMeta)Skull.getItemMeta();
+						
+						//sets skull owner as victims
+						skullMeta.setOwner(victim.getName());						
+						Skull.setItemMeta(skullMeta);
+						
+						//drops skull
+						e.getDrops().add(Skull);
+						
+						
+						//money reward
+			            EconomyResponse r = Guards.econ.depositPlayer(killer.getName(), plugin.getConfig().getInt("moneyOnGuardKill"));
+			            if(r.transactionSuccess()) {
+			               killer.sendMessage(String.format("You were rewarded %s for killing a guard", Guards.econ.format(r.amount)));
+			            } else {
+			                killer.sendMessage(String.format("An error occured: %s", r.errorMessage));
+			            }
+						
+						for(Player user: plugin.getServer().getOnlinePlayers()) {
+							if (!(plugin.onDuty.contains(user.getName()))) {
+								user.sendMessage(plugin.prefix +killer.getName() + " has just killed guard " + victim.getName());
+							}
+							
+						}
+						return;
+					} 
+					else
+					{
+						plugin.logger.info("Head Dropping Disabled");
+					}		
 			}
+			
+			
 			if(plugin.onDuty.contains(killer.getName())) {
 				e.getDrops().clear();
 				return;
 			}
+			
 			
 			int n = plugin.getConfig().getInt("playerHeadFrequency");
 			int rand = generator.nextInt(n);
