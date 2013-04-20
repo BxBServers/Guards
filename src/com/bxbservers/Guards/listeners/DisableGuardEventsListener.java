51d7ebfbb7089e52d19ed247cfc55b3c4c3fddc8
aet2505 <aet2505@gmail.com>
2013-04-20 19:50:42 +0100

    Commented and made DisableGuardsEvents more readable

--- src/com/bxbservers/Guards/listeners/DisableGuardEventsListener.java
+++ src/com/bxbservers/Guards/listeners/DisableGuardEventsListener.java
@@ -20,22 +20,34 @@ public class DisableGuardEventsListener implements Listener{
 
 	private Guards plugin;
 	
-	public DisableGuardEventsListener(Guards instance){
+	public DisableGuardEventsListener(Guards instance)
+	{
 		this.plugin = instance;
 	}
 	
 	//Stop Guards Moving Items in inventory
 	@EventHandler
 	public void onClick(InventoryClickEvent e){
-	    if(e.getWhoClicked() instanceof Player){
-	        Player player = (Player) e.getWhoClicked(); 
-	        if (player.getGameMode()== GameMode.CREATIVE){
+	    if(e.getWhoClicked() instanceof Player)
+	    {
+	    	//Get Player who clicked
+	        Player player = (Player) e.getWhoClicked();
+	        String name = player.getName();
+	        
+	        //If user is in Creative return
+	        if (player.getGameMode()== GameMode.CREATIVE)
+	        {
 	        	return;
 	        }
-	        String name = player.getName();
+
+	        //If User is on Duty
 	        if (plugin.onDuty.contains(name)) {
+	        	
+	        	//Cancel Event
 	        	e.setCancelled(true);
 	        	e.setCursor(new ItemStack(Material.AIR));
+	        	
+	        	//Close Inventory and Notify User
 	        	player.closeInventory();
 	        	player.sendMessage(plugin.prefix +"You Cannot use your Inventory while on Duty");
 	        }
@@ -44,10 +56,16 @@ public class DisableGuardEventsListener implements Listener{
 	
 	//Stop Guards Breaking Blocks
 	@EventHandler
-	public void onBreak(BlockBreakEvent e){
+	public void onBreak(BlockBreakEvent e)
+	{
+		//Get Player Info
 		Player player = e.getPlayer();    
 		String name = player.getName();
-	        if (plugin.onDuty.contains(name)) {
+		
+		//If player on Duty
+	        if (plugin.onDuty.contains(name)) 
+	        {
+	        	//Cancel event and Notify User
 	        	e.setCancelled(true);
 	        	player.sendMessage(plugin.prefix +"You Cannot Break Blocks while on Duty");
 	        }
@@ -55,10 +73,16 @@ public class DisableGuardEventsListener implements Listener{
 	
 	//Stop Guards Placing Blocks
 	@EventHandler
-	public void onPlace(BlockPlaceEvent e){
+	public void onPlace(BlockPlaceEvent e)
+	{
+		//Get Player Info
 		Player player = e.getPlayer();    
 		String name = player.getName();
-	        if (plugin.onDuty.contains(name)) {
+		
+		//If player on Duty
+	        if (plugin.onDuty.contains(name))
+	        {
+	        	//Cancel event and Notify user
 	        	e.setCancelled(true);
 	        	player.sendMessage(plugin.prefix +"You Cannot Place Blocks while on Duty");
 	        }
@@ -66,23 +90,37 @@ public class DisableGuardEventsListener implements Listener{
 	
 	//Stop Guards Dropping Blocks
 	@EventHandler
-	public void onDrop(PlayerDropItemEvent e){
-	    Player player = e.getPlayer();    
+	public void onDrop(PlayerDropItemEvent e)
+	{
+		//Get Player Info
+		Player player = e.getPlayer();    
 		String name = player.getName();
-	        if (plugin.onDuty.contains(name)) {
-	        		if (!(e.getItemDrop().getEntityId() == 144)){
-	        				e.setCancelled(true);
-	        				player.sendMessage(plugin.prefix +"You Cannot Drop Items while on Duty");
-	        		}
-	        }
+		
+		//If Player On duty
+        if (plugin.onDuty.contains(name))
+        {
+        //If it is not a player head	
+        	if (!(e.getItemDrop().getEntityId() == 144))
+        	{
+       			//cancel event and notify user
+        		e.setCancelled(true);
+        		player.sendMessage(plugin.prefix +"You Cannot Drop Items while on Duty");
+        	}
+        }
 	}
 	
 	//Stop Guards Picking up Items
 	@EventHandler
-	public void onPickup(PlayerPickupItemEvent e){
+	public void onPickup(PlayerPickupItemEvent e)
+	{
+		//Get Player Info
 		Player player = e.getPlayer();    
 		String name = player.getName();
-	        if (plugin.onDuty.contains(name)) {
+		
+		//If Player on Duty
+	        if (plugin.onDuty.contains(name)) 
+	        {
+	        	//Cancel Event
    				e.setCancelled(true);
 	        }
 	        
@@ -90,11 +128,21 @@ public class DisableGuardEventsListener implements Listener{
 	
 	//Stop Guards Right Clicking with Equipment
 	@EventHandler
-	public void equipmentClick(PlayerInteractEvent e) {
+	public void equipmentClick(PlayerInteractEvent e) 
+	{
+		//Get Player Info
 		Player player = e.getPlayer();
-		if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR){
+		
+		//If a right Click action
+		if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR)
+		{
+			//get Item Stack
 			ItemStack stack = player.getItemInHand();
-			if (stack.getTypeId()==397 || stack.getTypeId() == 384 ){
+			
+			//If a Special Item
+			if (stack.getTypeId()==397 || stack.getTypeId() == 384 )
+			{
+				//Cancel Event and return Item
 				e.setCancelled(true);
 				stack.setAmount(1);
 				player.getInventory().setItemInHand(stack);
